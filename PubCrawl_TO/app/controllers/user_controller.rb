9cab28bb7a9702b902134cc23e2746c95fed1606
Amin Shafiee <shafiee.amin@gmail.com>
2016-07-04 16:20:36 -0400

    added and completed new create show update destroy and private params for user controller

--- PubCrawl_TO/app/controllers/user_controller.rb
+++ PubCrawl_TO/app/controllers/user_controller.rb
@@ -1,4 +1,35 @@
 class UserController < ApplicationController
 before_action :authenticate_user!
 
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new(restaurant_params)
+end
+
+
+def show
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+
+end
+
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :neighbourhood, :profile, :firstname, :lastname, :DOB)
+  end
+
+
+
 end
